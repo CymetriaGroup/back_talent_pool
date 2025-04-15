e6f1708d8e4b6b446c0d5fbfd9f6b4896fc93a56
Leonardo Ramírez <juanlov4321@gmail.com>
2025-04-15 18:19:59 -0500

    Uncomment findById method in CompaniesRepository interface

--- src/domain/ports/companies.respository.ts
+++ src/domain/ports/companies.respository.ts
@@ -2,7 +2,7 @@ import { Companies } from "../entities/companies.entities";
 import { CreateCompaniesDTO } from "../dtos/companies/companies.dto";
 
 export interface CompaniesRepository {
-	// findById(id: string): Promise<Companies | null>;
+	findById(id: string): Promise<Companies | null>;
 	findByNit(nit: string): Promise<Companies | null>;
 	create(company: CreateCompaniesDTO): Promise<Companies>;
 	// delete(id: string): Promise<void>;
