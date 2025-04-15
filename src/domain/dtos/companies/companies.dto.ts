c4afc076d676a7585bbf5a46fbe10a7ac627cbf7
Leonardo Ramírez <juanlov4321@gmail.com>
2025-04-15 15:30:31 -0500

    Add CreateCompaniesDTO interface for company creation data structure

---
+++ src/domain/dtos/companies/companies.dto.ts
@@ -0,0 +1,12 @@
+export interface CreateCompaniesDTO {
+	commercial_name: string;
+	social_reason: string;
+	nit: string;
+	sector: string;
+	country: string;
+	city: string;
+	address: string;
+	phone: string;
+	number_of_employees: number;
+	number_of_annual_vacancies: number;
+}
