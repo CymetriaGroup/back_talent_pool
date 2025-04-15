68a904369ad68d18c0fd07a2e4b16d661a429ca3
Leonardo Ramírez <juanlov4321@gmail.com>
2025-04-15 15:30:26 -0500

    Add CompaniesModel to Sequelize configuration for database integration

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { UserModel } from "../infrastructure/db/sequelize/models/user.model";
+import { CompaniesModel } from "../infrastructure/db/sequelize/models/companies.model";
 import { ENVS_APP, ENVS_DB } from "./envs.config";
 
 export const sequelize = new Sequelize({
@@ -9,7 +10,7 @@ export const sequelize = new Sequelize({
 	username: ENVS_DB.DB_USER,
 	password: ENVS_DB.DB_PASSWORD,
 	database: ENVS_DB.DB_NAME,
-	models: [UserModel],
+	models: [UserModel, CompaniesModel],
 	timezone: "-05:00",
 	logging: ENVS_APP.IS_PRODUCTION ? console.log : true,
 });
