3433ba062aa360bba67d943df07bba7f53552c8d
Leonardo Ramírez <juanlov4321@gmail.com>
2025-04-15 15:31:26 -0500

    Add CompaniesController to handle company creation requests

---
+++ src/infrastructure/http/controllers/companies.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { CompaniesService } from "../services/companies.service";
+import { inject, injectable } from "tsyringe";
+import { createCompaniesSchema } from "../../../domain/dtos/companies/companies.schema";
+import { HttpException } from "../errors/HttpException";
+
+@injectable()
+export class CompaniesController {
+	constructor(
+		@inject("CompaniesService")
+		private companiesService: CompaniesService
+	) {}
+	async createCompanies(req: Request, res: Response) {
+		const { error, value } = createCompaniesSchema.validate(req.body, {
+			abortEarly: false,
+		});
+		if (error) {
+			throw new HttpException(
+				400,
+				error.details.map((detail) => detail.message)
+			);
+		}
+		const companies = await this.companiesService.create(value);
+		return res.status(201).json({ ok: true, companies });
+	}
+}
