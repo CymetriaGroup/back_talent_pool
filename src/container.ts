e26179c038dfbbc5c9d001751c5a18e71d4a2d99
Leonardo Ramírez <juanlov4321@gmail.com>
2025-04-15 15:30:18 -0500

    Register CompaniesRepository and CompaniesService in the dependency container

--- src/container.ts
+++ src/container.ts
@@ -1,7 +1,20 @@
 import { container } from "tsyringe";
+// injection for user repository
 import { UserRepository } from "./domain/ports/UserRepository";
 import { UserRepositorySequelize } from "./infrastructure/db/sequelize/repositories/UserRepositorySequelize";
+// inyection for companies repository
+import { CompaniesRepository } from "./domain/ports/companies.respository";
+import { CompaniesRepositorySequelize } from "./infrastructure/db/sequelize/repositories/companies.repository.sequelize";
+import { CompaniesService } from "./infrastructure/http/services/companies.service";
 
 container.register<UserRepository>("UserRepository", {
 	useClass: UserRepositorySequelize,
+});
+
+container.register<CompaniesRepository>("CompaniesRepository", {
+	useClass: CompaniesRepositorySequelize,
+});
+
+container.register<CompaniesService>("CompaniesService", {
+	useClass: CompaniesService,
 });
