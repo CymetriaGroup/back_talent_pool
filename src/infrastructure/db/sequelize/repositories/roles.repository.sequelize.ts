098e005c4a0864f2f3564b780c9dbcb7b486e203
Leonardo Ramírez <juanlov4321@gmail.com>
2025-05-13 17:26:16 -0500

    Add optional transaction support to findAll method in RolesRepository

--- src/infrastructure/db/sequelize/repositories/roles.repository.sequelize.ts
+++ src/infrastructure/db/sequelize/repositories/roles.repository.sequelize.ts
@@ -1,6 +1,7 @@
 import { RolesRepository } from "../../../../domain/ports/roles.repository";
 import { Roles } from "../../../../domain/entities/roles.entities";
 import { RolesModel } from "../models/roles.model";
+import { Transaction } from "sequelize";
 
 export class RolesRepositorySequelize implements RolesRepository {
 	async findById(id: string): Promise<Roles | null> {
@@ -17,6 +18,15 @@ export class RolesRepositorySequelize implements RolesRepository {
 				return null;
 			}
 			return new Roles(role.name, role.description, role.id);
+		});
+	}
+
+	async findAll(options?: { transaction?: Transaction }): Promise<Roles[] | null> {
+		return RolesModel.findAll(options).then((roles) => {
+			if (!roles) {
+				return null;
+			}
+			return roles.map((role) => new Roles(role.name, role.description, role.id));
 		});
 	}
 }
