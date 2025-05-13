20b3ab24bcfa2c019e3244f2ca9263e9e2301a3b
Leonardo Ramírez <juanlov4321@gmail.com>
2025-05-13 17:25:20 -0500

    Refactor CompaniesRepository create method to accept optional transaction options

--- src/domain/ports/companies.respository.ts
+++ src/domain/ports/companies.respository.ts
@@ -1,9 +1,16 @@
 import { Companies } from "../entities/companies.entities";
 import { CreateCompaniesDTO } from "../dtos/companies/companies.dto";
+import { CompaniesRegisterWithUserDTO } from "../dtos/companies/companies-register-with-user.dto";
+import type { Transaction } from "sequelize";
 
 export interface CompaniesRepository {
 	findById(id: string): Promise<Companies | null>;
 	findByNit(nit: string): Promise<Companies | null>;
-	create(company: CreateCompaniesDTO): Promise<Companies>;
+	create(
+		company: CreateCompaniesDTO,
+		options?: {
+			transaction?: Transaction;
+		}
+	): Promise<Companies>;
 	// delete(id: string): Promise<void>;
 }
