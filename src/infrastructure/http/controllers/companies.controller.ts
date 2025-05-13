ef2ec616ac92e986aa7bd2ac0888769b040eb495
Leonardo Ramírez <juanlov4321@gmail.com>
2025-05-13 17:26:30 -0500

    Add createCompaniesWithUser method to CompaniesController for user-specific company creation

--- src/infrastructure/http/controllers/companies.controller.ts
+++ src/infrastructure/http/controllers/companies.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { CompaniesService } from "../services/companies.service";
 import { inject, injectable } from "tsyringe";
 import { createCompaniesSchema } from "../../../domain/dtos/companies/companies.schema";
+import { createCompaniesWithUserSchema } from "../../../domain/dtos/companies/companies-register-with-user.schema";
 import { HttpException } from "../errors/HttpException";
 
 @injectable()
@@ -21,6 +22,19 @@ export class CompaniesController {
 			);
 		}
 		const companies = await this.companiesService.create(value);
+		return res.status(201).json({ ok: true, companies });
+	}
+	async createCompaniesWithUser(req: Request, res: Response) {
+		const { error, value } = createCompaniesWithUserSchema.validate(req.body, {
+			abortEarly: false,
+		});
+		if (error) {
+			throw new HttpException(
+				400,
+				error.details.map((detail) => detail.message)
+			);
+		}
+		const companies = await this.companiesService.createWithUser(value);
 		return res.status(201).json({ ok: true, companies });
 	}
 }
