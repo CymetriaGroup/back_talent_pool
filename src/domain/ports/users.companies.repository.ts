24bbef8d9658d0946f0ccbcc508bf9449e1a89a2
Leonardo Ramírez <juanlov4321@gmail.com>
2025-05-13 17:25:40 -0500

    Add optional transaction support to create method in UserCompaniesRepository

--- src/domain/ports/users.companies.repository.ts
+++ src/domain/ports/users.companies.repository.ts
@@ -1,8 +1,14 @@
 import { UserCompanies } from "../entities/users.companies.entities";
 import { UserCompaniesCreateDTO } from "../dtos/user_companies/user.companies.dto";
+import type { Transaction } from "sequelize";
 
 export interface UserCompaniesRepository {
-	create(user: UserCompaniesCreateDTO): Promise<UserCompanies>;
+	create(
+		user: UserCompaniesCreateDTO,
+		options?: {
+			transaction?: Transaction;
+		}
+	): Promise<UserCompanies>;
 	findById?(id: string): Promise<UserCompanies | null>;
 	findByEmail?(email: string): Promise<UserCompanies | null>;
 }
