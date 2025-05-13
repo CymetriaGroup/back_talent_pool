6b10840d900b78bdc04c1e32cf51a5a5cd153bd0
Leonardo Ramírez <juanlov4321@gmail.com>
2025-05-13 17:24:13 -0500

    Add PlansModel and SuscriptionsModel to Sequelize configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,6 +3,8 @@ import { CompaniesModel } from "../infrastructure/db/sequelize/models/companies.
 import { RolesModel } from "../infrastructure/db/sequelize/models/roles.model";
 import { UserCompaniesModel } from "../infrastructure/db/sequelize/models/user.companies.model";
 import { ENVS_APP, ENVS_DB } from "./envs.config";
+import { PlansModel } from "../infrastructure/db/sequelize/models/plans.model";
+import { SuscriptionsModel } from "../infrastructure/db/sequelize/models/suscriptions.model";
 
 export const sequelize = new Sequelize({
 	dialect: "mysql",
@@ -11,7 +13,7 @@ export const sequelize = new Sequelize({
 	username: ENVS_DB.DB_USER,
 	password: ENVS_DB.DB_PASSWORD,
 	database: ENVS_DB.DB_NAME,
-	models: [CompaniesModel, RolesModel, UserCompaniesModel],
+	models: [CompaniesModel, RolesModel, UserCompaniesModel, PlansModel, SuscriptionsModel],
 	timezone: "-05:00",
 	logging: ENVS_APP.IS_PRODUCTION ? console.log : true,
 });
